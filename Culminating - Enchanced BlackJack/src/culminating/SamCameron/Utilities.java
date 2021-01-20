b8bf4f96c52c3bdb8fbe5d016f4c3965b9b1a71a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 10:33:42 -0500

    Updated output of trivia.txt. Removed commented out numberChooser();

--- Culminating - Enchanced BlackJack/src/culminating/SamCameron/Utilities.java
+++ Culminating - Enchanced BlackJack/src/culminating/SamCameron/Utilities.java
@@ -7,23 +7,6 @@ public class Utilities {
 
     private static final Scanner sc = new Scanner(System.in);
 
-    // Possible use later
-    /*// Make sure the number provided by the user is valid
-    public static int numberChooser(int num, ArrayList<Integer> options, int upper_bound) {
-    
-    if(!(options.contains(num)) || num > upper_bound) {
-    System.out.print("Please enter an acceptable number (one of the options provided): ");
-    num = sc.nextInt();
-    
-    // Consume the new line
-    sc.nextLine();
-    
-    numberChooser(num, options, upper_bound);
-    }
-    
-    return num;
-    }*/
-
     public static String stringChooser(String input, ArrayList<String> options) {
 
         if(!options.contains(input)) {
